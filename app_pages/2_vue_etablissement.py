410e2117d9a965137131fca617cc41677da86654
Mathieu Bartozzi <mathieu.bartozzi@mlfmonde.org>
2026-01-12 18:46:36 +0100

    removing shcool names for more  privacy

--- app_pages/2_vue_etablissement.py
+++ app_pages/2_vue_etablissement.py
@@ -6,6 +6,8 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # Import explicite recommandé (évite les collisions de noms)
 from utils.functions import *
+from utils.functions import PLOTLY_CONFIG
+
 
 # -------------------------------------------------
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
